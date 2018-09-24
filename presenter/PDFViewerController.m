679cea036c5d20df5fe80be5892370659bbf4f29
Yuji <yuji.tachikawa@ipmu.jp>
2018-09-24 17:53:36 +0900

    better PDF drawing.

--- presenter/PDFViewerController.m
+++ presenter/PDFViewerController.m
@@ -20,43 +20,34 @@
 }
 -(NSImage*)imageAtPageNumber:(NSUInteger)pageNumber
 {
-    CGPDFPageRef pdfPage = CGPDFDocumentGetPage (currentPDF, pageNumber);
-    CGRect cropBox= CGPDFPageGetBoxRect(pdfPage, kCGPDFCropBox) ;
-    CGRect mediaBox= CGPDFPageGetBoxRect(pdfPage, kCGPDFMediaBox) ;
-    CGRect target=imageView.frame;
-    float ratio=target.size.height/cropBox.size.height;
-    float wratio=target.size.width/cropBox.size.width;
-    if(wratio<ratio)ratio=wratio;
-    float theight=ratio*cropBox.size.height;
-    float twidth=ratio*cropBox.size.width;
-    unsigned char *bitmap = malloc(target.size.width * target.size.height * sizeof(unsigned char) *4);
-    CGColorSpaceRef deviceRGB=CGColorSpaceCreateDeviceRGB();
-    CGContextRef context =CGBitmapContextCreate(bitmap,
-                                                target.size.width,
-                                                target.size.height,
-                                                8,
-                                                target.size.width * 4,
-                                                deviceRGB,
-                                                kCGImageAlphaPremultipliedFirst);
-    CGColorSpaceRelease(deviceRGB);
-    CGContextSetCMYKFillColor(context, 0, 0, 0, 0, 1);
-//    CGContextTranslateCTM(context, 0,target.size.height);
-//    CGContextScaleCTM(context, 1, -1);
-    CGContextTranslateCTM(context, (target.size.width-twidth)/2,(target.size.height-theight)/2);
-    CGContextFillRect(context,CGRectMake(0, 0, twidth, theight));
-    CGContextScaleCTM(context, ratio, ratio);
-    CGContextTranslateCTM(context, -(mediaBox.size.width-cropBox.size.width),-(mediaBox.size.height-cropBox.size.height));
-    //    CGContextClipToRect(context, CGRectMake(0,0, cropBox.size.width, cropBox.size.height));
-    CGContextDrawPDFPage (context, pdfPage);
-    CGImageRef cgImage = CGBitmapContextCreateImage(context);
-    CGContextRelease(context);
-    free(bitmap);
-    NSImage*image=[[NSImage alloc] initWithCGImage:cgImage size:NSZeroSize];
-    CGImageRelease(cgImage);
+    NSImage*image=[NSImage imageWithSize:imageView.frame.size flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+        CGPDFPageRef pdfPage = CGPDFDocumentGetPage (currentPDF, pageNumber);
+        CGRect cropBox= CGPDFPageGetBoxRect(pdfPage, kCGPDFCropBox) ;
+        CGRect mediaBox= CGPDFPageGetBoxRect(pdfPage, kCGPDFMediaBox) ;
+        CGRect target=dstRect;
+        float ratio=target.size.height/cropBox.size.height;
+        float wratio=target.size.width/cropBox.size.width;
+        if(wratio<ratio)ratio=wratio;
+        float theight=ratio*cropBox.size.height;
+        float twidth=ratio*cropBox.size.width;
+        CGContextRef context=[NSGraphicsContext currentContext].CGContext;
+        CGContextSetCMYKFillColor(context, 0, 0, 0, 0, 1);
+        //    CGContextTranslateCTM(context, 0,target.size.height);
+        //    CGContextScaleCTM(context, 1, -1);
+        CGContextTranslateCTM(context, (target.size.width-twidth)/2,(target.size.height-theight)/2);
+        CGContextFillRect(context,CGRectMake(0, 0, twidth, theight));
+        CGContextScaleCTM(context, ratio, ratio);
+        CGContextTranslateCTM(context, -(mediaBox.size.width-cropBox.size.width),-(mediaBox.size.height-cropBox.size.height));
+        //    CGContextClipToRect(context, CGRectMake(0,0, cropBox.size.width, cropBox.size.height));
+        CGContextDrawPDFPage (context, pdfPage);
+        return YES;
+    }];
     return image;
 }
 -(void)loadPDF:(NSURL*)fileURL
 {
+    self.window.representedURL=fileURL;
+    self.window.title=fileURL.lastPathComponent;
     if(currentPDF){
         CGPDFDocumentRelease(currentPDF);
     }
